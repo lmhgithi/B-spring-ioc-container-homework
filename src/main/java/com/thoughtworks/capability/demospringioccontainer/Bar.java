333bc3d11197b20c66ce3238563c70fdf4a4d94a
lmhgithi <lmhfox@foxmail.com>
2020-09-16 17:40:22 +0800

    [lmh] resolve

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
@@ -1,14 +1,23 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @Component
 public class Bar {
 
-    private Foo foo;
+    private final Foo foo;
 
-    public Bar(Foo foo) {
+    public Bar(Foo foo){
         this.foo = foo;
+    }
+
+    @PostConstruct
+    public void init() {
+        foo.setBar(this);
     }
 
     public void hi() {
